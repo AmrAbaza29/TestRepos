dd1bf3f242cd2de9bf3b82b610601f79467082a7
AmrAbaza29 <134836859+AmrAbaza29@users.noreply.github.com>
2024-04-19 19:36:12 +0200

    change programcs

--- TestRepos/Program.cs
+++ TestRepos/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace TestRepos
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Creating conflicts - Amr");
         }
     }
 }
